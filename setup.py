49217c2faa650b33e0ff22d82dddd63ae3e79d09
Dirk Makerhafen <1283167+dirk-attraktor@users.noreply.github.com>
2021-03-01 22:40:46 +0100

    updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="pyhtmlgui",
-    version="1.0",
+    version="1.1",
     author="Dirk Makerhafen",
     author_email="dirk@makerhafen.de",
     description="A Python library for building user interfaces",
