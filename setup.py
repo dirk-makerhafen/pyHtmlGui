58f7dfe026d3ce125b5a7f7369e6993bf7a21562
Dirk <Dirk@Computer.local>
2022-02-14 00:51:31 +0100

    updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="pyhtmlgui",
-    version="2.1",
+    version="2.2",
     author="Dirk Makerhafen",
     author_email="dirk@makerhafen.de",
     description="A Python library for building user interfaces",
